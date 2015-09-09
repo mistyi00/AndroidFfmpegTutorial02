79002dc987a2927d2c9945bdfad7127565d08fd7
mistyi <mistyi@naver.com>
2015-09-09 18:56:52 +0900

    Initial commit

---
+++ app/src/main/jni/Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := tutorial02
+LOCAL_SRC_FILES := tutorial02.c
+LOCAL_LDLIBS := -llog -ljnigraphics -lz -landroid
+LOCAL_SHARED_LIBRARIES := libavformat libavcodec libswscale libavutil libswresample
+
+include $(BUILD_SHARED_LIBRARY)
+$(call import-module,ffmpeg-2.7.2/android/arm)
---
+++ app/src/main/obj/local/armeabi/objs/tutorial02/tutorial02.o.d
@@ -0,0 +1,280 @@
+obj/local/armeabi/objs/tutorial02/tutorial02.o: jni/tutorial02.c \
+ jni/kr_co_joytune_androidffmpegtutorial02_MainActivity.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/jni.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/cdefs.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/android/api-level.h \
+ D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavcodec/avcodec.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/errno.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/errno.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm/errno.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm-generic/errno.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm-generic/errno-base.h \
+ D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/samplefmt.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/stdint.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/_types.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/machine/_types.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/_wchar_limits.h \
+ D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/avutil.h \
+ D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/common.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/inttypes.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/limits.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/limits.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/limits.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/machine/internal_types.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/machine/limits.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/syslimits.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm/page.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/math.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/types.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/posix_types.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/stddef.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/compiler.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm/posix_types.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm/types.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/types.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/machine/kernel.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/stdio.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/stdlib.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/string.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/malloc.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/alloca.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/strings.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/memory.h \
+ D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/attributes.h \
+ D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/version.h \
+ D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/macros.h \
+ D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/avconfig.h \
+ D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/mem.h \
+ D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/error.h \
+ D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/rational.h \
+ D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/mathematics.h \
+ D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/intfloat.h \
+ D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/log.h \
+ D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/pixfmt.h \
+ D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/old_pix_fmts.h \
+ D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/attributes.h \
+ D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/avutil.h \
+ D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/buffer.h \
+ D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/cpu.h \
+ D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/channel_layout.h \
+ D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/dict.h \
+ D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/frame.h \
+ D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/buffer.h \
+ D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/dict.h \
+ D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/samplefmt.h \
+ D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/log.h \
+ D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/pixfmt.h \
+ D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/rational.h \
+ D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavcodec/version.h \
+ D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/version.h \
+ D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavcodec/old_codec_ids.h \
+ D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavformat/avformat.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/time.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/time.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/time.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm/siginfo.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm-generic/siginfo.h \
+ D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavformat/avio.h \
+ D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/common.h \
+ D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavformat/version.h \
+ D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libswscale/swscale.h \
+ D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libswscale/version.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/pthread.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/signal.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm/signal.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm-generic/signal.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm/sigcontext.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/ucontext.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/user.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sched.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/android/native_window.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/android/rect.h \
+ D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/android/native_window_jni.h
+
+jni/kr_co_joytune_androidffmpegtutorial02_MainActivity.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/jni.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/cdefs.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/android/api-level.h:
+
+D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavcodec/avcodec.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/errno.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/errno.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm/errno.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm-generic/errno.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm-generic/errno-base.h:
+
+D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/samplefmt.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/stdint.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/_types.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/machine/_types.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/_wchar_limits.h:
+
+D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/avutil.h:
+
+D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/common.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/inttypes.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/limits.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/limits.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/limits.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/machine/internal_types.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/machine/limits.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/syslimits.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm/page.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/math.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/types.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/posix_types.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/stddef.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/compiler.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm/posix_types.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm/types.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/types.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/machine/kernel.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/stdio.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/stdlib.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/string.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/malloc.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/alloca.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/strings.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/memory.h:
+
+D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/attributes.h:
+
+D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/version.h:
+
+D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/macros.h:
+
+D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/avconfig.h:
+
+D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/mem.h:
+
+D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/error.h:
+
+D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/rational.h:
+
+D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/mathematics.h:
+
+D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/intfloat.h:
+
+D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/log.h:
+
+D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/pixfmt.h:
+
+D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/old_pix_fmts.h:
+
+D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/attributes.h:
+
+D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/avutil.h:
+
+D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/buffer.h:
+
+D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/cpu.h:
+
+D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/channel_layout.h:
+
+D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/dict.h:
+
+D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/frame.h:
+
+D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/buffer.h:
+
+D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/dict.h:
+
+D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/samplefmt.h:
+
+D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/log.h:
+
+D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/pixfmt.h:
+
+D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/rational.h:
+
+D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavcodec/version.h:
+
+D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/version.h:
+
+D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavcodec/old_codec_ids.h:
+
+D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavformat/avformat.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/time.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/time.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/time.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm/siginfo.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm-generic/siginfo.h:
+
+D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavformat/avio.h:
+
+D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavutil/common.h:
+
+D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libavformat/version.h:
+
+D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libswscale/swscale.h:
+
+D:/Software/android/android-ndk-r10e/sources/ffmpeg-2.7.2/android/arm/include/libswscale/version.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/pthread.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/signal.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm/signal.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm-generic/signal.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm/sigcontext.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/ucontext.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/user.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sched.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/android/native_window.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/android/rect.h:
+
+D:/Software/android/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/android/native_window_jni.h:
